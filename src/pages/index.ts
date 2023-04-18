1299bb834a6a8a776dc3227d34095971582ea5e6
pinonesteban@gmail.com <ar-esteban.pinon@rakuten.com>
2023-04-18 13:11:46 +0200

    empy branch

--- src/pages/index.ts
+++
@@ -1,9 +0,0 @@
-import HomePage from "./HomePage";
-import ProductPage from "./ProductPage";
-import FAQPage from "./FAQPage";
-import RegulatorPage from "./RegulatorPage";
-import UseCasePage from "./UseCasePage";
-import DocsPage from "./DocsPage";
-import WhitePaperPage from "./WhitePaperPage";
-
-export { HomePage, ProductPage, FAQPage, RegulatorPage, UseCasePage, DocsPage, WhitePaperPage };
